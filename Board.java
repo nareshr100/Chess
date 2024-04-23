8fe4f22d338e9722427d23b373f441ea1b599b34
nareshr100 <naresh@suriar.me.uk>
2024-04-23 17:43:37 +0100

    Click on square to print whether a piece is there

--- Board.java
+++ Board.java
@@ -1,25 +1,102 @@
 import java.io.IOException;
+import java.util.List;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 import Pieces.IGameView;
+import Pieces.Piece;
+import Pieces.PiecePosition;
 
 
 
-public class Board {
+public class Board implements MouseListener, MouseMotionListener{
 
     private static final int BOARD_LENGTH = BoardPanel.PANEL_LENGTH*BoardPanel.SQUARE_LENGTH;
     private JFrame frame = new JFrame();
+    private JPanel panel;
+    private IGameView gameView;
+    // Container pane = frame.getContentPane();
 
     public Board(IGameView gameView) throws IOException {
+        this.gameView = gameView;
 
+        frame = new JFrame();
+        frame.addMouseListener(this);
+        frame.addMouseMotionListener(this);
         frame.setUndecorated(false);
-        JPanel panel = new BoardPanel(gameView);
+
+        panel = new BoardPanel(gameView);
         frame.setBounds(10, 10, BOARD_LENGTH, BOARD_LENGTH);
         frame.add(panel);
         frame.setDefaultCloseOperation(3);
         frame.setVisible(true);
     }
 
+    public void mouseDragged(MouseEvent e) {
+        // System.out.println("You have DRAGGED the mouse");
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        // System.out.println("You have MOVED the mouse");
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        final Point pos = e.getPoint();
+        final PiecePosition clickPiecePos = convertToPiecePosition(GraphicsPosition.create(pos.y, pos.x));
+        List<Piece> pieces = gameView.getPieces();
+        printPieceAtPosition(pieces, clickPiecePos);
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        System.out.println("Mouse has ENTERED the panel.");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        System.out.println("Mouse has EXITED the panel.");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        //System.out.println("You have RELEASED the mouse");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        //System.out.println("You have PRESSED the mouse");
+    }
+
+    private static PiecePosition convertToPiecePosition(GraphicsPosition pos){
+        return new PiecePosition(scaleCoordinate(pos.getRow(), true), scaleCoordinate(pos.getCol(), false));
+    }
+    
+    private static int scaleCoordinate(int coord, boolean isRow) {
+
+        if (isRow) {
+            return (int) (coord-30)/BoardPanel.SQUARE_LENGTH;
+        }
+        else {
+            return (int) (coord/BoardPanel.SQUARE_LENGTH);
+        }
+    }
+
+    private static void printPieceAtPosition(List<Piece> pieces, PiecePosition clickPiecePos) {
+        boolean pieceHere = false;
+        for (Piece piece: pieces){
+            PiecePosition piecePos = piece.getPos();
+            String colour = piece.isWhite() ? "White" : "Black";
+            if (piecePos.getRow() == clickPiecePos.getRow() & piecePos.getCol() == clickPiecePos.getCol()) {
+                System.out.println("There is a " + colour +" "+ piece.getPieceType() + " here");
+                pieceHere = true;
+                break;
+            }
+        }
+        if (!pieceHere) {
+            System.out.println("There is no piece here");
+        }
+    }
 }
--- ChessGame.java
+++ ChessGame.java
@@ -1,62 +1,13 @@
-
-
-
-import java.awt.Image;
-import java.awt.event.MouseListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.util.LinkedList;
-import javax.imageio.ImageIO;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 import Pieces.*;
 
 public class ChessGame {
     
     public static void main(String[] args) throws IOException {
-        
-        
-        
-        // Piece bKnight=new Piece(1, 0, false, "knight", ps);
-        // Piece bbishop=new Piece(2, 0, false, "bishop", ps);
-        // Piece bqueen=new Piece(3, 0, false, "queen", ps);
-        // Piece bking=new Piece(4, 0, false, "king", ps);
-        // Piece bbishop2=new Piece(5, 0, false, "bishop", ps);
-        // Piece bkight2=new Piece(6, 0, false, "knight", ps);
-        // Piece brook2=new Piece(7, 0, false, "rook", ps);
-        // Piece bpawn1=new Piece(1, 1, false, "pawn", ps);
-        // Piece bpawn2=new Piece(2, 1, false, "pawn", ps);
-        // Piece bpawn3=new Piece(3, 1, false, "pawn", ps);
-        // Piece bpawn4=new Piece(4, 1, false, "pawn", ps);
-        // Piece bpawn5=new Piece(5, 1, false, "pawn", ps);
-        // Piece bpawn6=new Piece(6, 1, false, "pawn", ps);
-        // Piece bpawn7=new Piece(7, 1, false, "pawn", ps);
-        // Piece bpawn8=new Piece(0, 1, false, "pawn", ps);
-        
-        // Piece wRook = Rook.create(PiecePosition.create(0, 7), true);
-
-        // Piece wKnight=new Piece(1, 7, true, "knight", ps);
-        // Piece wbishop=new Piece(2, 7, true, "bishop", ps);
-        // Piece wqueen=new Piece(3, 7, true, "queen", ps);
-        // Piece wking=new Piece(4, 7, true, "king", ps);
-        // Piece wbishop2=new Piece(5, 7, true, "bishop", ps);
-        // Piece wkight2=new Piece(6, 7, true, "knight", ps);
-        // Piece wrook2=new Piece(7, 7, true, "rook", ps);
-        // Piece wpawn1=new Piece(1, 6, true, "pawn", ps);
-        // Piece wpawn2=new Piece(2, 6, true, "pawn", ps);
-        // Piece wpawn3=new Piece(3, 6, true, "pawn", ps);
-        // Piece wpawn4=new Piece(4, 6, true, "pawn", ps);
-        // Piece wpawn5=new Piece(5, 6, true, "pawn", ps);
-        // Piece wpawn6=new Piece(6, 6, true, "pawn", ps);
-        // Piece wpawn7=new Piece(7, 6, true, "pawn", ps);
-        // Piece wpawn8=new Piece(0, 6, true, "pawn", ps);
 
         IGameView gameView = new GameState();
         Board board = new Board(gameView);
                 
-        
-        
     }
 }
